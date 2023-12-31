d65b0e8a7054621ff7b6dabbe300590d94d69a6e
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-31 15:35:05 +0000

    erlang: step 0

---
+++ src/erlang/step0_repl.erl
@@ -0,0 +1,20 @@
+-module(step0_repl).
+-export([main/1]).
+
+main(A) ->
+  case io:get_line("user> ") of
+    eof -> io:fwrite("eof");
+    {error, _} -> io:fwrite("error somewhere");
+    Line ->
+      io:nl(), %% Required on Windows
+      io:fwrite("~s", [rep(string:chomp(Line))]),
+      io:nl(),
+      main(A)
+  end.
+
+rep(X) -> print(eval(read(X))).
+
+read(X) -> X.
+eval(X) -> X.
+print(X) -> X.
+           
