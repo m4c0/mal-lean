ed2c8dc50c90af3977424d0dab290c51af29a354
Eduardo Costa <m4c0@users.noreply.github.com>
2023-12-31 16:41:43 +0000

    reader: strings and comments

--- src/erlang/reader.erl
+++ src/erlang/reader.erl
@@ -11,8 +11,25 @@ tokenise([C|Str], Toks)
 
 tokenise("~@" ++ Str, Toks) -> tokenise(Str, ["~@"|Toks]);
 
-tokenise([C|Str], Toks)
-  when C == $[; C == $]; C == ${; C == $}; C == $(; C == $);
-       C == $'; C == $`; C == $^; C == $~; C == $@
-       -> tokenise(Str, [[C]|Toks]).
+tokenise([Chr|Str], Toks) ->
+  case Chr of
+    C when C == $[; C == $]; C == ${; C == $}; C == $(; C == $);
+           C == $'; C == $`; C == $^; C == $~; C == $@
+           -> tokenise(Str, [[C]|Toks]);
+    $" ->
+      case take_str(Str, [$"]) of
+        {ValidStr, Rest} -> tokenise(Rest, [ValidStr|Toks])
+      end;
+    $; ->
+      case take_comment(Str, [$;]) of
+        {Cmt, Rest} -> tokenise(Rest, [Cmt|Toks])
+      end
+  end.
 
+take_str("", _) -> error;
+take_str([$"|Str], Result) -> {lists:reverse([$"|Result]), Str};
+take_str([C|Str], Result) -> take_str(Str, [C|Result]).
+
+take_comment("", Result) -> {lists:reverse(Result), ""};
+take_comment([$\n|Str], Result) -> {lists:reverse(Result), Str};
+take_comment([C|Str], Result) -> take_comment(Str, [C|Result]).
