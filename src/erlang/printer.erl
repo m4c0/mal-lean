0b657b1f2edd52471e9fd64e16f22b7759f65376
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-02 16:54:42 +0000

    printer - some list support

--- src/erlang/printer.erl
+++ src/erlang/printer.erl
@@ -3,5 +3,8 @@
 
 pr_str(error) -> "error";
 pr_str({symbol, S}) -> S;
-pr_str({number, N}) -> "Number " ++ N;
-pr_str({list, L}) -> "List " ++ L.
+pr_str({number, N}) -> N;
+pr_str({list, L}) -> "(" ++ print_list(L) ++ ")";
+pr_str(X) -> io_lib:format("error: unknown result~n       ~p", [X]).
+
+print_list(L) -> lists:join(" ", lists:map(fun pr_str/1, L)).
