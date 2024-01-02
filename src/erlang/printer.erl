6bcce10bc26735822ae0a15953164e4da87f4413
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-02 18:32:08 +0000

    printer - hashmap

--- src/erlang/printer.erl
+++ src/erlang/printer.erl
@@ -9,9 +9,15 @@ pr_str({string, S}, false) -> S;
 pr_str({string, S}, true) -> print_str(S);
 pr_str({list, L}, R) -> "(" ++ print_list(L, R) ++ ")";
 pr_str({vector, L}, R) -> "[" ++ print_list(L, R) ++ "]";
+pr_str({hashmap, L}, R) -> "{" ++ print_map(L, R) ++ "}";
 pr_str(X, _) -> io_lib:format("error: unknown result~n       ~p", [X]).
 
 print_list(L, R) -> string:join(lists:map(fun (S) -> pr_str(S, R) end, L), " ").
+
+print_map(M, R) ->
+  Fn = fun (K, V, Acc) -> [K,V|Acc] end,
+  L = maps:fold(Fn, [], M),
+  print_list(L, R).
 
 print_str(S) -> lists:reverse(print_str(S, [$"])).
 print_str([], Acc) -> [$"|Acc];
