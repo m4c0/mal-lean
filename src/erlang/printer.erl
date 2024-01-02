c3385ad1054af5d6dfb4dd3d81ff717c28f29e1e
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-02 17:11:27 +0000

    printer - fix string join

--- src/erlang/printer.erl
+++ src/erlang/printer.erl
@@ -7,4 +7,4 @@ pr_str({number, N}) -> N;
 pr_str({list, L}) -> "(" ++ print_list(L) ++ ")";
 pr_str(X) -> io_lib:format("error: unknown result~n       ~p", [X]).
 
-print_list(L) -> lists:join(" ", lists:map(fun pr_str/1, L)).
+print_list(L) -> string:join(lists:map(fun pr_str/1, L), " ").
