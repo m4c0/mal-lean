5f2586b6eab4501b5787cbc7aee710d370f51d5e
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-03 14:31:26 +0000

    step 4 - prn

--- src/erlang/core.erl
+++ src/erlang/core.erl
@@ -2,10 +2,12 @@
 -export([ns/0]).
 
 ns() ->
-  #{"+" => {lambda, fun add/1},
-    "-" => {lambda, fun sub/1},
-    "*" => {lambda, fun mult/1},
-    "/" => {lambda, fun dv/1}}.
+  maps:map(fun (_, V) -> {lambda, V} end,
+           #{"+" => fun add/1,
+             "-" => fun sub/1,
+             "*" => fun mult/1,
+             "/" => fun dv/1,
+             "prn" => fun prn/1}).
 
 add([{number, A},{number, B}]) -> {number, A+B};
 add(_) -> {error, "invalid parameters"}.
@@ -18,3 +20,8 @@ mult(_) -> {error, "invalid parameters"}.
 
 dv([{number, A},{number, B}]) -> {number, A div B};
 dv(_) -> {error, "invalid parameters"}.
+
+prn([]) -> {nil, nil};
+prn([X|Xs]) -> 
+  io:format("~s~n", [printer:pr_str(X, false)]),
+  prn(Xs).
