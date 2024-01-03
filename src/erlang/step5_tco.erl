65d065ff3bc8cd4d3e2fa3030e4217a182dd9219
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-03 17:52:31 +0000

    step 5 - copy-pasta

---
+++ src/erlang/step5_tco.erl
@@ -0,0 +1,122 @@
+-module(step5_tco).
+-export([main/1]).
+
+main(_) ->
+  Env = env:new(nil),
+  maps:foreach(fun (K, V) -> env:set(Env, K, V) end, core:ns()),
+  rep("(def! not (fn* (a) (if a false true)))", Env),
+  repl(Env).
+
+repl(Env) ->
+  case io:get_line("user> ") of
+    eof -> io:fwrite("eof");
+    {error, X} -> io:fwrite("error: ~s", [X]);
+    Line ->
+      io:nl(), %% Required on Windows
+      io:fwrite("~s", [rep(Line, Env)]),
+      io:nl(),
+      repl(Env)
+  end.
+
+%% read-eval-print
+
+rep(X, Env) -> print(eval(read(X), Env)).
+
+read(X) -> reader:read_str(X).
+
+eval({seq, list, V}, Env) -> eval_list(V, Env);
+eval(X, Env) -> eval_ast(X, Env).
+
+print(X) -> printer:pr_str(X, true).
+
+%% eval_ast
+
+eval_ast({symbol, X}, Env) ->
+  case env:get(Env, X) of
+    {ok, V} -> V;
+    error -> {error, io_lib:format("~s not found", [X])}
+  end;
+eval_ast({seq, Seq, L}, Env) ->
+  Fn = fun (V, Acc) when is_list(Acc) ->
+           case eval(V, Env) of
+             {error, X} -> {error, X};
+             X -> [X|Acc]
+           end;
+           (_, X) -> X
+       end,
+  case lists:foldl(Fn, [], L) of
+    NL when is_list(NL) -> {seq, Seq, lists:reverse(NL)};
+    X -> X
+  end;
+eval_ast({hashmap, M}, Env) ->
+  Fn = fun (K, V, Acc) when is_map(Acc) ->
+           case eval(V, Env) of
+             {error, X} -> {error, X};
+             X -> Acc#{K => X}
+           end;
+           (_, _, X) -> X
+       end,
+  case maps:fold(Fn, #{}, M) of
+    NM when is_map(NM) -> {hashmap, NM};
+    X -> X
+  end;
+eval_ast(X, _) -> X.
+
+%% others
+
+bind([], _) -> ok;
+bind([{symbol, K},V|L], Env) ->
+  case eval(V, Env) of
+    {error, X} -> {error, X};
+    VV -> env:set(Env, K, VV),
+          bind(L, Env)
+  end;
+bind(_, _) -> {error, "invalid parameter pair"}.
+
+%% eval bits
+
+eval_list([], _) -> {seq, list, []};
+eval_list([{symbol, "do"}|L], Env) ->
+  case eval_ast({seq, list, L}, Env) of
+    {seq, list, NL} -> lists:last(NL);
+    X -> X
+  end;
+eval_list([{symbol, "if"},Cond,T], Env) -> eval_if(Cond, T, nil, Env);
+eval_list([{symbol, "if"},Cond,T,F], Env) -> eval_if(Cond, T, F, Env);
+eval_list([{symbol, "fn*"},{seq,_,Binds},Body], Env) ->
+  {lambda, fun (Exprs) -> 
+               case env:new(Env, Binds, Exprs) of
+                 {error, X} -> {error, X};
+                 NEnv -> eval(Body, NEnv)
+               end
+           end};
+eval_list([{symbol, "fn*"}|_], _) ->
+  {error, "invalid fn* signature"};
+eval_list([{symbol, "def!"},{symbol, K},V], Env) ->
+  case eval(V, Env) of
+    {error, X} -> {error, X};
+    VV -> env:set(Env, K, VV), VV
+  end;
+eval_list([{symbol, "def!"}|_], _) ->
+  {error, "invalid def! signature"};
+eval_list([{symbol, "let*"},{seq,_,As},P], Env) ->
+  NEnv = env:new(Env),
+  case bind(As, NEnv) of
+    ok -> eval(P, NEnv);
+    X -> X
+  end;
+eval_list([{symbol, "let*"}|_], _) ->
+  {error, "invalid let* signature"};
+eval_list(L, Env) ->
+  case eval_ast({seq, list, L}, Env) of
+    {seq, list, [{lambda, Fn}|NL]} -> Fn(NL);
+    X -> X
+  end.
+
+eval_if(Cond, T, F, Env) ->
+  case eval(Cond, Env) of
+    {boolean, false} -> eval(F, Env);
+    nil -> eval(F, Env);
+    {error, X} -> {error, X};
+    _ -> eval(T, Env)
+  end.
