bec05980326b0b6b29e0f15862a957343820dc78
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-04 17:24:24 +0000

    step A - host language

--- src/erlang/stepA_mal.erl
+++ src/erlang/stepA_mal.erl
@@ -8,6 +8,7 @@ main(As) ->
   rep("(def! load-file (fn* (f) (eval (read-string (str \"(do \" (slurp f) \"\\nnil)\")))))", Env),
   rep("(defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list 'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw \"odd number of forms to cond\")) (cons 'cond (rest (rest xs)))))))", Env),
   env:set(Env, "eval", {lambda, fun (X) -> eval_eval(X, Env) end}),
+  env:set(Env, "*host-language*", "erlang"),
   case As of
     [Fn|AA] -> 
       MA = lists:map(fun (A) -> {string, A} end, AA),
