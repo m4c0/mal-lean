a9e8adb32beaa68caa54a14936ff5be4b4f4a2a2
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-04 16:09:11 +0000

    core - most step 9 functions
    look, ma! without looking at the guide!

--- src/erlang/core.erl
+++ src/erlang/core.erl
@@ -12,16 +12,27 @@ ns() ->
              "<=" => fun lte/1,
              ">=" => fun gte/1,
              "=" => fun eq/1,
+             "assoc" => fun assoc/1,
              "atom" => fun atom/1,
              "atom?" => fun atomq/1,
              "concat" => fun concat/1,
              "cons" => fun cons/1,
+             "contains?" => fun contains/1,
              "count" => fun count/1,
              "deref" => fun deref/1,
+             "dissoc" => fun dissoc/1,
              "empty?" => fun empty/1,
+             "false?" => fun falseq/1,
              "first" => fun first/1,
+             "get" => fun geti/1,
+             "hash-map" => fun hashmap/1,
+             "keys" => fun keys/1,
+             "keyword" => fun keyword/1,
+             "keyword?" => fun keywordq/1,
              "list" => fun list/1,
              "list?" => fun listq/1,
+             "map?" => fun mapq/1,
+             "nil?" => fun nilq/1,
              "nth" => fun nth/1,
              "println" => fun println/1,
              "prn" => fun prn/1,
@@ -29,10 +40,17 @@ ns() ->
              "reset!" => fun reset/1,
              "rest" => fun rest/1,
              "read-string" => fun readstring/1,
+             "sequential?" => fun sequential/1,
              "slurp" => fun slurp/1,
              "str" => fun str/1,
+             "symbol" => fun symbol/1,
+             "symbol?" => fun symbolq/1,
              "swap!" => fun swap/1,
-             "vec" => fun vec/1}).
+             "true?" => fun trueq/1,
+             "vals" => fun vals/1,
+             "vec" => fun vec/1,
+             "vector" => fun vector/1,
+             "vector?" => fun vectorq/1}).
 
 add([{number, A},{number, B}]) -> {number, A+B};
 add(_) -> {error, "invalid parameters"}.
@@ -62,6 +80,11 @@ eq([{seq, _, A},{seq, _, B}]) -> seq_eq(A, B);
 eq([A,B]) -> {boolean, A == B};
 eq(_) -> {error, "invalid parameters for ="}.
 
+assoc([{hashmap, _}=E]) -> E;
+assoc([{hashmap, M},{T, _}=K,V|As]) when T == string; T == keyword ->
+  assoc([{hashmap, M#{K => V}}|As]);
+assoc(_) -> {error, "invalid parameters for assoc"}.
+
 atom([X]) ->
   Id = {atom, length(get())},
   put(Id, X),
@@ -80,6 +103,13 @@ concat(_, _) -> {error, "invalid parameters for concat"}.
 cons([V, {seq, _, L}]) -> {seq, list, [V|L]};
 cons(_) -> {error, "invalid parameters for cons"}.
 
+contains([{hashmap, M}, {T, _}=K]) when T == string; T == keyword ->
+  case maps:find(K, M) of
+    error -> {boolean, false};
+    _ -> {boolean, true}
+  end;
+contains(_) -> {error, "invalid parameters for contains?"}.
+
 count([{seq, _, L}]) -> {number, length(L)};
 count([nil]) -> {number, 0};
 count(_) -> {error, "invalid parameters for count"}.
@@ -87,20 +117,62 @@ count(_) -> {error, "invalid parameters for count"}.
 deref([{atom, _} = X]) -> get(X);
 deref(_) -> {error, "invalid parameters for deref"}.
 
+dissoc([{hashmap, _} = X]) -> X;
+dissoc([{hashmap, M},{T, _}=K|As]) when T == string; T == keyword ->
+  dissoc([{hashmap, maps:remove(K, M)}|As]);
+dissoc(_) -> {error, "invalid parameters for dissoc"}.
+
 empty([{seq, _, []}]) -> {boolean, true};
 empty([_]) -> {boolean, false};
 empty(_) -> {error, "invalid parameters for empty?"}.
+
+falseq([{boolean, false}]) -> {boolean, true};
+falseq([_]) -> {boolean, false};
+falseq(_) -> {error, "invalid parameters for false?"}.
 
 first([nil]) -> nil;
 first([{seq, _, []}]) -> nil;
 first([{seq, _, [E|_]}]) -> E;
 first(_) -> {error, "invalid parameters for first"}.
 
+geti([nil, _]) -> nil;
+geti([{hashmap, M},{T, _}=K]) when T == string; T == keyword ->
+  case maps:find(K, M) of
+    error -> nil;
+    {ok, V} -> V
+  end;
+geti(_) -> {error, "invalid parameters for get"}.
+
+hashmap(L) -> hashmap(L, #{}).
+hashmap([], Acc) -> {hashmap, Acc};
+hashmap([{T, _}=K,V|L], Acc) when T == string; T == keyword ->
+  hashmap(L, Acc#{K => V});
+hashmap(_, _) -> {error, "invalid parameters for hash-map"}.
+
+keys([{hashmap, M}]) -> {seq, list, maps:keys(M)};
+keys(_) -> {error, "invalid parameters for keys"}.
+
+keyword([{string, S}]) -> {keyword, ":" ++ S};
+keyword([{keyword, _} = X]) -> X;
+keyword(_) -> {error, "invalid parameters for keyword"}.
+
+keywordq([{keyword, _}]) -> {boolean, true};
+keywordq([_]) -> {boolean, false};
+keywordq(_) -> {error, "invalid parameters for keyword?"}.
+
 list(X) -> {seq, list, X}.
 
 listq([{seq, list, _}]) -> {boolean, true};
 listq([_]) -> {boolean, false};
 listq(_) -> {error, "invalid parameters for list?"}.
+
+mapq([{hashmap, _}]) -> {boolean, true};
+mapq([_]) -> {boolean, false};
+mapq(_) -> {error, "invalid parameters for map?"}.
+
+nilq([nil]) -> {boolean, true};
+nilq([_]) -> {boolean, false};
+nilq(_) -> {error, "invalid parameters for nil?"}.
 
 nth([{seq, _, L},{number, N}]) when N >= 0 -> nth(L, N);
 nth(_) -> {error, "invalid parameters for nth"}.
@@ -125,6 +197,10 @@ rest(_) -> {error, "invalid paramters for rest"}.
 readstring([{string, X}]) -> reader:read_str(X);
 readstring(_) -> {error, "invalid parameters for read-string"}.
 
+sequential([{seq, _, _}]) -> {boolean, true};
+sequential([_]) -> {boolean, false};
+sequential(_) -> {error, "invalid parameters for sequential?"}.
+
 slurp([{string, X}]) ->
   case file:read_file(X) of
     {ok, Bin} -> {string, binary_to_list(Bin)};
@@ -140,8 +216,28 @@ swap([{atom, _} = X,{lambda, Fn}|As]) ->
   V;
 swap(_) -> {error, "invalid parameters for swap"}.
 
+symbol([{string, S}]) -> {symbol, S};
+symbol(_) -> {error, "invalid parameters for symbol"}.
+
+symbolq([{symbol, _}]) -> {boolean, true};
+symbolq([_]) -> {boolean, false};
+symbolq(_) -> {error, "invalid parameters for symbol?"}.
+
+trueq([{boolean, true}]) -> {boolean, true};
+trueq([_]) -> {boolean, false};
+trueq(_) -> {error, "invalid parameters for true?"}.
+
+vals([{hashmap, M}]) -> {seq, list, maps:values(M)};
+vals(_) -> {error, "invalid parameters for vals"}.
+
 vec([{seq, _, L}]) -> {seq, vector, L};
 vec(_) -> {error, "invalid parameters for vec"}.
+
+vector(L) -> {seq, vector, L}.
+
+vectorq([{seq, vector, _}]) -> {boolean, true};
+vectorq([_]) -> {boolean, false};
+vectorq(_) -> {error, "invalid parameters for vector?"}.
 
 %% helpers
 
