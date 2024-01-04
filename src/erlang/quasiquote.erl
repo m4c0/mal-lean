606892829118bc0537a7d23ad728675f9280d3bd
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-04 11:10:40 +0000

    quasiquote - basic forms

--- src/erlang/quasiquote.erl
+++ src/erlang/quasiquote.erl
@@ -1,4 +1,7 @@
 -module(quasiquote).
 -export([expand/1]).
 
-expand(Ast) -> {error, "TBD"}.
+expand({seq, list, [{symbol, "unquote"}, E]}) -> E;
+expand({T, _} = X) when T == hashmap; T == symbol ->
+  {seq, list, [{symbol, "quote"}, X]};
+expand(X) -> X.
