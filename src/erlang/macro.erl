71135dfc3ea626f92beb39431ec3bbc193e2a436
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-04 14:21:42 +0000

    macro - works

--- src/erlang/macro.erl
+++ src/erlang/macro.erl
@@ -1,4 +1,9 @@
 -module(macro).
 -export([expand/2]).
 
+expand({seq, list, [{symbol, K}|As]} = X, Env) ->
+  case env:get(Env, K) of
+    {ok, {macro, Fn}} -> expand(Fn(As), Env);
+    _ -> X
+  end;
 expand(X, _) -> X.
