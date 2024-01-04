4677fdbe216197cac06ab7fbb1fdd8664af8678b
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-04 11:35:20 +0000

    quasiquote - something working

--- src/erlang/quasiquote.erl
+++ src/erlang/quasiquote.erl
@@ -2,6 +2,13 @@
 -export([expand/1]).
 
 expand({seq, list, [{symbol, "unquote"}, E]}) -> E;
+expand({seq, list, L}) -> qq(L, []);
 expand({T, _} = X) when T == hashmap; T == symbol ->
   {seq, list, [{symbol, "quote"}, X]};
 expand(X) -> X.
+
+qq([], Acc) -> {seq, list, []};
+qq([{seq, list, [{symbol, "splice-unquote"},E]}|L], Acc) -> 
+  {seq, list, [{symbol, "concat"},E,qq(L, Acc)]};
+qq([E|L], Acc) ->
+  {seq, list, [{symbol, "cons"},expand(E),qq(L, Acc)]}.
