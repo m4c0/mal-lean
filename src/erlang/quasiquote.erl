c2aa58142d1579d3d0b5a5e656fdf43beb08abf2
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-04 11:44:20 +0000

    quasiquote - pass step 7 tests

--- src/erlang/quasiquote.erl
+++ src/erlang/quasiquote.erl
@@ -2,13 +2,16 @@
 -export([expand/1]).
 
 expand({seq, list, [{symbol, "unquote"}, E]}) -> E;
-expand({seq, list, L}) -> qq(L, []);
+expand({seq, list, L}) -> qq(L);
 expand({T, _} = X) when T == hashmap; T == symbol ->
   {seq, list, [{symbol, "quote"}, X]};
 expand(X) -> X.
 
-qq([], Acc) -> {seq, list, []};
-qq([{seq, list, [{symbol, "splice-unquote"},E]}|L], Acc) -> 
-  {seq, list, [{symbol, "concat"},E,qq(L, Acc)]};
-qq([E|L], Acc) ->
-  {seq, list, [{symbol, "cons"},expand(E),qq(L, Acc)]}.
+%% This breaks TCO, but it is required to pass step-7 tests.
+%% Maybe there is a way to accumulate this in a TCO-friendly way, but the logic
+%% is already too confusing as-is
+qq([]) -> {seq, list, []};
+qq([{seq, list, [{symbol, "splice-unquote"},E]}|L]) -> 
+  {seq, list, [{symbol, "concat"},E,qq(L)]};
+qq([E|L]) ->
+  {seq, list, [{symbol, "cons"},expand(E),qq(L)]}.
